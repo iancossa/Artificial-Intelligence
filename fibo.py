fc5e59b648b5d266e2580c9109effe8b735f30d3
Ian Cossa <ianrafaelelias@gmail.com>
2025-08-16 17:56:00 +0530

    Fibonnaci

---
+++ fibo.py
@@ -0,0 +1,13 @@
+def fib(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            next_fib = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_fib)
+        return fib_sequence
+    
+print(fib(10))
