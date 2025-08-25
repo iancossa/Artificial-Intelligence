f3d5188d84c8f749a81d17ceeb6764fdfdcb547f
Ian Cossa <ianrafaelelias@gmail.com>
2025-08-26 00:55:12 +0530

    Visualizing the data

--- img_classification.py
+++ img_classification.py
@@ -3,4 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-#%matplotlib inline
+#
+data = pd.read_csv('content.csv')
+print(data.head())
+
+#extrating data from dataset 
+x = data.iloc[:,2:].values
+# Convert to numeric, replacing non-numeric values with 0
+x = pd.DataFrame(x).apply(pd.to_numeric, errors='coerce').fillna(0).values
+
+#reshaping the extracted data into a reasonable size
+x = x[0].reshape(int(np.sqrt(len(x[0]))), -1).astype('uint8')  # Use first row and calculate square dimensions
+plt.imshow(x, cmap='gray')
+plt.show()
+print(x)
